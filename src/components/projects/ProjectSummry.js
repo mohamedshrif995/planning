5a01a292e3e9f2339f1c5c195dbb408d1cb8fd08
mohamedshrif995 <mohamedshrif995@gmail.com>
2019-08-21 21:49:46 +0200

    --- Auto-Git Commit ---

---
+++ src/components/projects/ProjectSummry.js
@@ -0,0 +1,17 @@
+import React from 'react';
+const ProjectSummry=()=>{
+    return(
+           <div className="card z-depth-0 project-summary">
+                 <div className="card-content grey-text text-darken-3">
+                     <span className="card-text">
+                         project title
+                     </span>
+                     <p>
+                         created by:
+                     </p>
+                     <p className="grey-text">3d  </p>
+                 </div>
+            </div>
+    )
+}
+export default ProjectSummry
