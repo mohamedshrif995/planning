332ccddce161f1f63eaf56c114a7bdf2f4fa7ac3
mohamedshrif995 <mohamedshrif995@gmail.com>
2019-08-23 23:54:20 +0200

    add link to app.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { BrowserRouter ,Switch,Route} from 'react-router-dom'
 import NavBar from './components/layout/NavBar';
 import Dasboard from './components/dashbord/Dasboard';
+import ProjectDetails from './components/projects/ProjectDetails'
+import Signin from './components/auth/Signin';
+import SignUp from './components/auth/Signup';
+import CreateProject from './components/projects/CreateProject';
 
 function App() {
   return (
@@ -9,7 +13,15 @@ function App() {
       <div className="App">
        <NavBar />
        <Switch>
-         <Route path='/' component={Dasboard} />
+         <Route exact path='/' component={Dasboard} />
+         <Route path='/project/:id' component={ProjectDetails} />
+         <Route path='/signin' component={Signin} />
+         <Route path='/signup' component={SignUp} />
+         <Route path='/create' component={CreateProject} />
+
+
+
+
        </Switch>
       </div>
     </BrowserRouter>
