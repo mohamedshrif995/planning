d25fb94f06381fda2858f0d1348885e81fcd42f8
mohamedshrif995 <mohamedshrif995@gmail.com>
2019-08-23 23:56:20 +0200

    add links to signout links

--- src/components/layout/SignoutLInk.js
+++ src/components/layout/SignoutLInk.js
@@ -4,8 +4,8 @@ import {NavLink} from 'react-router-dom'
 const SignoutLink = () => {
     return (
         <ul className="right">
-            <li><NavLink to='/'>Sign up</NavLink></li>
-            <li><NavLink to='/'></NavLink>Login</li>
+            <li><NavLink to='/signup'>Sign up</NavLink></li>
+            <li><NavLink to='/signin'>Login</NavLink></li>
         </ul>
     );
 }
