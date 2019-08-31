731bcdfa859044b50b10c7f8b4690b80066762b5
mohamedshrif995 <mohamedshrif995@gmail.com>
2019-08-31 23:32:37 +0200

    add action to action reducer

--- src/store/actions/CreateProjectActions.js
+++ src/store/actions/CreateProjectActions.js
@@ -1,6 +1,17 @@
 export const CreateProjectActions=(project)=>{
-    return(dispatch,getState)=>{
-        
-        dispatch({type:'CREATE_PROJECT',project})
+    return(dispatch,getState,{getFirebase,getFirestore})=>{
+        const fireStore=getFirestore()
+        fireStore.collection('projects').add({
+            ...project,
+            authorFirstName:'mod',
+            authorLastName:'shan',
+            authorID:12344,
+            createdAt: new Date()
+        }).then(()=>{
+            dispatch({type:'CREATE_PROJECT',project})
+        }).catch((err)=>{
+            dispatch({type:'CREATE_PROJECT_ERROR',err})
+        })
+
     }
 }
