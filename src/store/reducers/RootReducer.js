27239e25bf869b11f6ddf153c1e116e3344ed2b7
mohamedshrif995 <mohamedshrif995@gmail.com>
2019-08-31 23:33:36 +0200

    add firestore and firebase

--- src/store/reducers/RootReducer.js
+++ src/store/reducers/RootReducer.js
@@ -1,9 +1,13 @@
 import { combineReducers } from "redux";
+import {firestoreReducer} from 'redux-firestore'
 import AuthReducer from './AuthReducer'
 import ProjectReducer from './ProjectReducer'
+import {firebaseReducer} from 'react-redux-firebase'
 
 const RootReducer=combineReducers({
 Auth:AuthReducer,
-Project:ProjectReducer
+Project:ProjectReducer,
+firestore:firestoreReducer,
+firebase:firebaseReducer
 })
 export default RootReducer
