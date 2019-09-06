db3e6298d906b777bdfd6abcc7ef617ababfe3d7
mohamedshrif995 <mohamedshrif995@gmail.com>
2019-09-06 21:19:17 +0200

    add redux to navbar

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -3,13 +3,14 @@ import { Link } from 'react-router-dom'
 import SigninLink from './SigninLink';
 import SignoutLink from './SignoutLInk';
 import {connect} from 'react-redux'
-const NavBar = () => {
+const NavBar = (props) => {
+    const{auth}=props
+    const links=auth.uid?<SigninLink />:<SignoutLink />
     return (
         <nav className="nav-wrapper grey darken-3">
             <div className="container">
                 <Link to='/' className='brand-logo'>my plan</Link>
-                <SigninLink />
-                <SignoutLink />
+              {links}
             </div>
         </nav>
     );
@@ -17,7 +18,7 @@ const NavBar = () => {
 const mapStateTiProps=(state)=>{
     console.log('my auth',state)
     return{
-        
+    auth:state.firebase.auth
     }
 }
 export default connect(mapStateTiProps) (NavBar);
