a2413cc2e4f943ea43ceda985dfc93c1ab1f0452
mohamedshrif995 <mohamedshrif995@gmail.com>
2019-09-06 21:18:56 +0200

    add redux to sign in

--- src/components/auth/Signin.js
+++ src/components/auth/Signin.js
@@ -1,43 +1,58 @@
 import React, { Component } from 'react'
-
- class Signin extends Component {
-     state={
-email:'',
-password:''
-     }
-     handeleChange=(e)=>{
-         this.setState({
-             [e.target.id]:e.target.value
-         })
-     }
-     handleSubmit=(e)=>{
-         e.preventDefault()
-         console.log('this.state', this.state)
-     }
+import { connect } from 'react-redux'
+import { signIn } from '../../store/actions/AuthActions'
+class Signin extends Component {
+    state = {
+        email: '',
+        password: ''
+    }
+    handeleChange = (e) => {
+        this.setState({
+            [e.target.id]: e.target.value
+        })
+    }
+    handleSubmit = (e) => {
+        e.preventDefault()
+        console.log('this.state', this.state)
+        this.props.signIn(this.state)
+    }
     render() {
+        const { authError } = this.props
         return (
             <div className="container">
                 <form onSubmit={this.handleSubmit} className="white">
                     <h5 className="grey-text text-darken-3">sign in</h5>
                     <div className="input-field">
                         <label htmlFor="email">Email</label>
-                        <input type="email" id="email"onChange={this.handeleChange}/>
+                        <input type="email" id="email" onChange={this.handeleChange} />
                     </div>
                     <div className="input-field">
                         <label htmlFor="password">Password</label >
-                        <input type="password" id="password" onChange={this.handeleChange}/>
+                        <input type="password" id="password" onChange={this.handeleChange} />
                     </div>
                     <div className="input-field">
                         <button className="btn pink ligthen-1 z-depth-0" onClick={this.handleSubmit}>
                             login
                         </button>
-                        </div>
+                    </div>
+                    <div className="red-text center">
 
+                        {authError ? <p>{authError}</p> : ''}
+                    </div>
                 </form>
-                
+
             </div>
         )
     }
 }
-
-export default Signin
+const mapStateToProps = (state) => {
+    return {
+        authError: state.Auth.authError
+    }
+}
+const mapDispatchToProps = (dispatch) => {
+    return {
+        signIn: (credentials) => dispatch(signIn(credentials))
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Signin)
