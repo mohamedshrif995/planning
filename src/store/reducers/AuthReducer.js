4941899e151c5ec851668de961d18f3065a32ffc
mohamedshrif995 <mohamedshrif995@gmail.com>
2019-09-06 21:20:30 +0200

    add auth reducers

--- src/store/reducers/AuthReducer.js
+++ src/store/reducers/AuthReducer.js
@@ -1,6 +1,28 @@
-const inintState=[]
+const inintState={
+    authError:null
+}
 
 const AuthReducer=(state=inintState,action)=>{
-return state
+    switch (action.type) {
+        case 'LOGIN_FAIL':
+            console.log('LOGIN_FAIL')
+            return {
+                ...state,
+                authError:'login fail'
+            }
+            case 'LOGIN_SUCCESS':
+                    console.log('LOGIN_SUCCESS')
+                
+           return{
+               ...state,
+               authError:null
+           }
+           case 'SIGN_OUT':
+               console.log('SIGN_OUT')
+               return state
+              
+           default:
+               return state
+    }
 }
 export default AuthReducer
