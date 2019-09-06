c5f6a32c42ab4f9e72adf2d51aa52aab7857980d
mohamedshrif995 <mohamedshrif995@gmail.com>
2019-09-06 21:19:38 +0200

    add redux to sign in links

--- src/components/layout/SigninLink.js
+++ src/components/layout/SigninLink.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom'
-const SigninLink = () => {
+import {signOut}from '../../store/actions/AuthActions'
+import { connect}from 'react-redux'
+const SigninLink = (props) => {
     return (
         <ul className="right">
             <li><NavLink to='/create'>New Project</NavLink></li>
-            <li><NavLink to='/'></NavLink>SignOut</li>
+            <li><a onClick={props.signOut} to='/'>SignOut</a></li>
             <li><NavLink to='/' className='btn btn-floating pink lighten-1'>nn</NavLink></li>
         </ul>
     ); 
 }
-
-export default SigninLink;
+const mapDispatchToProps=(dispatch)=>{
+    return{
+        signOut:()=>dispatch(signOut())
+        
+    }
+}
+export default connect(
+    null,mapDispatchToProps
+) (SigninLink);
