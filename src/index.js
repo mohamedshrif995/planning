364ef94e5e059cb416c32bef18d37a59112be2b3
mohamedshrif995 <mohamedshrif995@gmail.com>
2019-09-07 17:11:09 +0200

    add use profile to get profile data

--- src/index.js
+++ src/index.js
@@ -16,11 +16,13 @@ const store=createStore(RootReducer,
     compose(
     applyMiddleware(thunk.withExtraArgument({getFirestore,getFirebase})),
     reduxFirestore(fbconfig),
-    reactReduxFirebase(fbconfig)
+    reactReduxFirebase(fbconfig,{useFirestoreForProfile: true,userProfile: 'users',attachAuthIsReady:true})
     )
     )
+    store.firebaseAuthIsReady.then(()=>{
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+        ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+    })
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
